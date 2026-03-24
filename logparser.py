a5005d57275e73283ebc124902675079a43aec06
Nehaa Srinivasan <srinivasannehaa0@gmail.com>
2026-03-24 12:42:00 +0000

    log parser changes

--- logparser.py
+++ logparser.py
@@ -1,12 +1,34 @@
 """This script helps to parse the log of log.txt and detect suspicious count"""
-alert_count=0
+alert_count=0  # for different key word cpunt
+word_count=0
+word_count_unauthorised=0
+word_count_failed=0
 with open('log.txt')  as f: # open file to read log.txt
    for i, j in enumerate(f, start=1):   # iterating through lines and assigning numbers
-      j=j.strip()                       # remove spacing
-      if "ERROR" in j or "UNAUTHORIZED" in j or "FAILED" in j:  # check for specific keywords
-         alert_count+=1                                               # count the number of times they have occured
-         print('ALERT!', i, j)                                  # alert message
-print(' total number of alerts is :', alert_count)                    # total count of alerts
+      j=j.strip() 
+      dict_add={i:j}
+      for values in dict_add.values():
+         list_values=[values] # convert the values to list
+         for lvalue in list_values: 
+            splitvalue=lvalue.split() # to split the each word in a list
+            if 'IP' in splitvalue: # check of for IP word index
+               indexip=splitvalue.index('IP')
+               indexip_add=indexip+1 # get ip_Add index
+               words=['ERROR','UNAUTHORIZED','FAILED']
+               if any(w in splitvalue for w in words):  # check for specific keywords are present in the list
+                  alert_count+=1 # get the total alert count
+               if 'ERROR' in splitvalue: # individual key word count
+                     word_count+=1
+               if 'UNAUTHORIZED'in splitvalue:
+                  word_count_unauthorised+=1
+               if 'FAILED' in splitvalue:
+                  word_count_failed+=1                                                              # count the number of times they have occured
+               print('ALERT!',splitvalue[indexip],splitvalue[indexip_add],'These ips are suscpicious')
+                                          
+print('Therefore, the total number of alerts are :', alert_count)   
+print('Error has occured: ',word_count)
+print('Unauthorised has occured',word_count_unauthorised)
+print('FAILED has occured',word_count_failed)                
 
       
       
